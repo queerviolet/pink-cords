3b0065819b5d2659b32b5323cb58831880ea5ddb
mrweckel <mrweckel@gmail.com>
2015-02-28 16:33:34 -0500

    add functionality for color change on mouse move

--- javascripts/malgorythm.js
+++ javascripts/malgorythm.js
@@ -1,1 +1,8 @@
-T("sin").play();
+$(document).mousemove(function(e){
+    var $width = ($(document).width())/255;
+    var $height = ($(document).height())/255;
+    var $pageX = parseInt(e.pageX / $width,10);
+    var $pageY = parseInt(e.pageY / $height,10);
+    var nodeKlass = document.getElementsByClassName("node")
+    $(nodeKlass).css("fill", "rgb("+$pageX+","+$pageY+","+$pageX+")");
+});

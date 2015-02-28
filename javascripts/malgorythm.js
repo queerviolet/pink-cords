0c57eeba0789520d16e44d7f290e97ef451cc323
mrweckel <mrweckel@gmail.com>
2015-02-28 18:07:56 -0500

    remove malgorythms js

--- javascripts/malgorythm.js
+++
@@ -1,8 +0,0 @@
-$(document).mousemove(function(e){
-    var $width = ($(document).width())/255;
-    var $height = ($(document).height())/255;
-    var $pageX = parseInt(e.pageX / $width,10);
-    var $pageY = parseInt(e.pageY / $height,10);
-    var nodeKlass = document.getElementsByClassName("node")
-    $(nodeKlass).css("fill", "rgb("+$pageX+","+$pageY+","+$pageX+")");
-});
